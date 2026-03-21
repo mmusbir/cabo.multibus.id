605b9c6b223d5b3c406a1bd9113b51e3571ad28a
Muhammad Musbir <mmusbirr@gmail.com>
2026-03-22 06:36:53 +0800

    Enable PDO_ATTR_EMULATE_PREPARES to support Supabase/Vercel Transaction mode connection pooling

--- config/db.php
+++ config/db.php
@@ -20,7 +20,7 @@ try {
     $conn = new PDO($dsn, $db_user, $db_pass, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::ATTR_EMULATE_PREPARES   => true,
     ]);
 
     // Set timezone to UTC+8 (WITA)
