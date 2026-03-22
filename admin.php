cff6a985cdbc54070ea5cf307ef55d943964867d
Muhammad Musbir <mmusbirr@gmail.com>
2026-03-23 00:13:55 +0800

    Clean legacy admin manifest styles

--- admin.php
+++ admin.php
@@ -785,7 +785,7 @@ if (!isset($_REQUEST['action'])):
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="assets/css/admin.css?v=10">
+  <link rel="stylesheet" href="assets/css/admin.css?v=11">
   <link rel="stylesheet" href="assets/css/navbar.css?v=10">
   <link rel="stylesheet" href="assets/css/admin-bootstrap.css?v=2">
   <style>
@@ -2545,20 +2545,6 @@ Harga: ${price}`;
     };
   </script>
   <style>
-    @media (max-width: 899px) {
-      #passengerList {
-        flex-direction: column !important;
-        align-items: stretch !important;
-        justify-content: flex-start !important;
-      }
-
-      .seat-block {
-        width: 100% !important;
-        margin-bottom: 12px !important;
-        display: block !important;
-      }
-    }
-
     /* Responsive styles moved to includes/navbar.php */
   </style>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
