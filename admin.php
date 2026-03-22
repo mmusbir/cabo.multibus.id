29ca7cf294720f5630579fb9ead832e14cf9d6aa
Muhammad Musbir <mmusbirr@gmail.com>
2026-03-23 01:31:29 +0800

    Consolidate admin styles into bootstrap layer

--- admin.php
+++ admin.php
@@ -785,8 +785,7 @@ if (!isset($_REQUEST['action'])):
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="assets/css/admin.css?v=11">
-  <link rel="stylesheet" href="assets/css/admin-bootstrap.css?v=4">
+  <link rel="stylesheet" href="assets/css/admin-bootstrap.css?v=5">
   <style>
     /* iOS Safari Auto-Zoom Prevention */
     @media (max-width: 768px) {
