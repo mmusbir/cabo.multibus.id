535756ce63805613c278b5e7febda62d4f7ce368
Muhammad Musbir <mmusbirr@gmail.com>
2026-03-26 13:32:34 +0800

    Enable shared theme asset caching

--- login.php
+++ login.php
@@ -80,7 +80,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <link rel="stylesheet" href="assets/css/theme-toggle.css?v=12">
+    <link rel="stylesheet" href="assets/css/theme-toggle.css?v=13">
     <script id="tailwind-config">
         tailwind.config = {
             darkMode: "class",
